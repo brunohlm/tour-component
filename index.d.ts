465126ff2077303ec98f8a2f0e6ec31ad626eaa6
Bruno Melo <brunohlm@gmail.com>
2017-07-20 11:04:32 -0300

    exportando highlight e step

--- index.d.ts
+++ index.d.ts
@@ -1,2 +1,4 @@
 export { TourComponentModule } from './src/components/tour/tour.module';
-export { TourController } from './src/components/tour/tour'
+export { TourController } from './src/components/tour/tour';
+export { Step } from './src/components/tour/step';
+export { Highlight } from './src/components/tour/highlight';
